da5327e616f29f8eefa860143507c278f1e6e27d
mate <netboot@msn.com>
2015-12-04 12:41:33 +0100

    fix load

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
-window.addEventListener()
-{
-  // prendo il nodi con tag box sotto forma di array
-  var boxes = document.getElementsByClassName("box");
-  console.log(boxes.length)
-  // TODO: non va dentro il for
-  // FIXME: boxes.length sempre 0
-  var i;
-  for(i = 0; i < boxes.length; i++){
-    var b = boxes[i];
-    //inserisce una velocita
-    b.dx=i+1;
-    b.dy=1;
-  }
-  // restituisce la posizione di una pallina
-  function getPos(el){
-    var x = (el.style.top == "") ? 0 : parseInt(el.style.top)
-    var y = (el.style.left == "")  ? 0 : parseInt(el.style.left)
-    return {'x':x, 'y':y}
-  }
-  var interval = setInterval(function(){
-      for(var i = 0; i<boxes.length; i++){
-        var b = boxes[i]
-        var pos = getPos(b)
-        //console.log(pos)
-        pos.x += b.dx
-        pos.y += b.dy
-        b.style.left = pos.x+'px'
-        b.style.top = pos.y+'px'
-        //console.log(pos)
+var interval = 0
+window.addEventListener('load',
+  function()
+  {
+    // prendo il nodi con tag box sotto forma di array
+    var boxes = document.getElementsByClassName("box");
+    var env = document.getElementById("scatola")
+    var i;
+    for(i = 0; i < boxes.length; i++){
+      var b = boxes[i];
+      //inserisce una velocita
+      b.dx=i+1;
+      b.dy=1;
+    }
+    // restituisce la posizione di una pallina
+    function getPos(el){
+      var x = (el.style.top == "") ? 0 : parseInt(el.style.top)
+      var y = (el.style.left == "")  ? 0 : parseInt(el.style.left)
+      return {'x':x, 'y':y}
+    }
+    function AABBcollision(posA,posB){
+      return posA.x > posB.x || posA.y > posB.y 
+    }
+    function updateDir(el){
+      var posEnv = getPos(env)
+      var pos = getPos(el)
+      if (AABBcollision(pos,posEnv)){
+        // risoluzione collisione
+        
+          
+      }else{
+        //no collisione
+          pos.x += el.dx
+          pos.y += el.dy
       }
-  },30);
-  //setInterval(function(){ alert("Hello"); }, 3000);
-}
+      
+          b.style.left = pos.x+'px'
+          b.style.top = pos.y+'px'
+    }
+    
+    interval = setInterval(function(){
+        for(var i = 0; i<boxes.length; i++){
+          var b = boxes[i]
+          updateDir(boxes,env)
+        }
+    },30);
+  })
+// function stopAnimate(){
+//   clearInterval(interval)
+// }
