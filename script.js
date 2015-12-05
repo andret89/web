f0a6b11779d5cf8781c2e53ac3c651af94e50c8e
acer <acer@nebook.com>
2015-12-05 02:56:01 +0100

    fix collision

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var interval = 0
+
 window.addEventListener('load',
   function()
   {
@@ -18,33 +19,67 @@ window.addEventListener('load',
       var y = (el.style.left == "")  ? 0 : parseInt(el.style.left)
       return {'x':x, 'y':y}
     }
-    function AABBcollision(posA,posB){
-      return posA.x > posB.x || posA.y > posB.y 
+    
+    function getInfo(el){
+      var x = (el.style.top == "") ? 0 : parseInt(el.style.top)
+      var y = (el.style.left == "")  ? 0 : parseInt(el.style.left)
+      var w = (el.style.width  == "") ? 0 : parseInt(el.style.width)
+      var h = (el.style.height == "")  ? 0 : parseInt(el.style.height)
+      return {'x':x, 'y':y, 'width':w, 'height':h }
+
     }
-    function updateDir(el){
-      var posEnv = getPos(env)
-      var pos = getPos(el)
-      if (AABBcollision(pos,posEnv)){
-        // risoluzione collisione
-        
-          
-      }else{
-        //no collisione
-          pos.x += el.dx
-          pos.y += el.dy
-      }
+
+    function AABBcollision(a,b){
+      var rect1 = getInfo(b)
+      var rect2 = getInfo(a)
+      console.log('rect1',rect1,'rect2',rect2)
       
-          b.style.left = pos.x+'px'
-          b.style.top = pos.y+'px'
+      if (rect1.x < rect2.x + rect2.width &&
+        rect1.x + rect1.width > rect2.x &&
+        rect1.y < rect2.y + rect2.height &&
+        rect1.height + rect1.y > rect2.y) {
+          // collision detected!
+        return true
+      }
+      return false
     }
     
+    function updateDir(el){
+      var pos = getPos(el)
+
+      if (!AABBcollision(el,env)){
+        // risoluzione collisione
+        if (el.style.background == "black")
+          el.style.background = "red"
+        else
+          el.style.background = "black"
+        //$('#box').color("black");  
+      }
+      if (el.style.background == "black"){
+        //no collisione
+        pos.x -= el.dx
+        pos.y -= el.dy
+      }
+      else{
+        pos.x += el.dx
+        pos.y += el.dy
+      }   
+      
+      b.style.left = pos.x+'px'
+      b.style.top = pos.y+'px'
+      
+    }
     interval = setInterval(function(){
         for(var i = 0; i<boxes.length; i++){
           var b = boxes[i]
-          updateDir(boxes,env)
+          updateDir(b)
         }
     },30);
   })
-// function stopAnimate(){
-//   clearInterval(interval)
+
+function stopAnimate(){
+  clearInterval(interval)
+}
+// function startAnimate(){
+//   setInterval(interval,30)
 // }
