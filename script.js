f8b589c0827d72fd50ff6634123309b49ff4c464
mate <netboot@msn.com>
2015-12-07 21:54:49 +0100

    fix collision first

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ window.addEventListener('load',
   function()
   {
     // prendo il nodi con tag box sotto forma di array
-    var boxes = document.getElementsByClassName("box");
+    var boxes = new Array()
     var env = document.getElementById("scatola")
+    var box = document.getElementsByClassName("box");
     var i;
-    for(i = 0; i < boxes.length; i++){
-      var b = boxes[i];
+    for(i = 0; i < box.length; i++){
+      var b = box[i];
       //inserisce una velocita
       b.dx=i+1;
       b.dy=1;
+      boxes.push(b)
     }
     // restituisce la posizione di una pallina
     function getPos(el){
@@ -19,70 +21,46 @@ window.addEventListener('load',
       var y = (el.style.left == "")  ? 0 : parseInt(el.style.left)
       return {'x':x, 'y':y}
     }
-<<<<<<< HEAD
-    // TODO: gestione collisioni
-    function AABBcollision(posA,posB){
-      return posA.x > posB.x || posA.y > posB.y 
-=======
     
     function getInfo(el){
       var x = (el.style.top == "") ? 0 : parseInt(el.style.top)
       var y = (el.style.left == "")  ? 0 : parseInt(el.style.left)
       var w = (el.style.width  == "") ? 0 : parseInt(el.style.width)
       var h = (el.style.height == "")  ? 0 : parseInt(el.style.height)
-      return {'x':x, 'y':y, 'width':w, 'height':h }
-
+      return {'x':x, 'y':y, 'w':w, 'h':h }
     }
-
-    function AABBcollision(a,b){
-      var rect1 = getInfo(b)
-      var rect2 = getInfo(a)
-      console.log('rect1',rect1,'rect2',rect2)
-      
-      if (rect1.x < rect2.x + rect2.width &&
-        rect1.x + rect1.width > rect2.x &&
-        rect1.y < rect2.y + rect2.height &&
-        rect1.height + rect1.y > rect2.y) {
-          // collision detected!
-        return true
-      }
-      return false
->>>>>>> 535d9f6df8d5a7bf07e0c4e9fc89ef179eb7cc72
-    }
+       
     
-    function updateDir(el){
-      var pos = getPos(el)
-
-      if (!AABBcollision(el,env)){
-        // risoluzione collisione
-        if (el.style.background == "black")
-          el.style.background = "red"
-        else
-          el.style.background = "black"
-        //$('#box').color("black");  
-      }
-      if (el.style.background == "black"){
-        //no collisione
-        pos.x -= el.dx
-        pos.y -= el.dy
-      }
-      else{
-        pos.x += el.dx
-        pos.y += el.dy
-      }   
-      
-      b.style.left = pos.x+'px'
-      b.style.top = pos.y+'px'
-      
-    }
     interval = setInterval(function(){
         for(var i = 0; i<boxes.length; i++){
-          var b = boxes[i]
-          updateDir(b)
+          var bb = boxes[i]
+          //updateDir(b)
+              
+        // TODO: gestione collisioni
+          var b = getInfo(bb)
+          if (b.x + b.w + bb.dx > bb.parentNode.offsetWidth-4 || b.x + bb.dx +2 <= 0) bb.dx *= -1;
+          if (b.y + b.h + bb.dy > bb.parentNode.offsetHeight-4 || b.y + bb.dy +2 <= 0) bb.dy *= -1;
+          b.x += bb.dx;
+          b.y += bb.dy;
+          bb.style.left = b.x+'px'
+          bb.style.top = b.y+'px'
         }
     },30);
+    // TODO: collision dt per le altre palline
+    env.addEventListener('mouseup', function(e){
+      var p = document.createElement('div')
+      p.className = 'box';
+      //cambiare la posizion
+      // p.style.left=e.x - p.offsetLeft +'px'
+      // p.style.top=e.y - p.offsetTop +'px'
+      p.dx=1;
+      p.dy=1;
+      env.appendChild(p)
+      boxes.push(p)
+      
+    })
   })
-
+  
 function stopAnimate(){
   clearInterval(interval)
 }
